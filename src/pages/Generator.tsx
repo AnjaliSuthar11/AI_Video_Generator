fe3790fd4d3f9139515112bb0307b4aaf9848ae6
Your Name <anjalisuthar114@gmail.com>
2026-04-17 18:52:05 +0530

    updated

--- src/pages/Generator.tsx
+++ src/pages/Generator.tsx
@@ -58,27 +58,6 @@ const Generator = () => {
             </div>
 
       </form>
-      <form onSubmit={handleGenerate} className="max-w-4xl mx-auto mb-40">
-        <Title heading="Create In-Context Image" description="Upload your model and product images to generate stunning UGC, short-form videos and social media posts"/>
-
-            <div className="flex gap-20 max-sm:flex-col items-start justify-between">
-
-                <div className="flex flex-col w-full sm:max-w-60 gap-8 mt-8 mb-12">
-                   <UploadZone file={productImage} onClear={()=>setProductImage(null)} onChange={(e)=>handleFileChange(e,"product")} label="Product Image"/>
-                   <UploadZone file={modelImage} onClear={()=>setModelImage(null)} onChange={(e)=>handleFileChange(e,"model")} label="Model Image"/>
-                  
-                </div>
-{/* right col */}
-                <div className="w-full ">
-                  <div className="mb-4">
-                    <label htmlFor="name" className="block text-sm mb-4">Project Name</label>
-                    <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Name Your Project" required className="w-full bg-white/3 rounded-lg border-2 p-4 text-sm border-violet-200/10 focus:border-violet-500/50 outline-none transition-all"/>
-                  </div>
-                  
-                </div>
-            </div>
-
-      </form>
     </div>
   )
 }
